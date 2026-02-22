e12adb8e441f666a623bf912aba1491934128483
piergiorgiodanti <piergiorgio.danti@edu.unifi.it>
2026-02-22 12:11:12 +0100

    ChessEval

---
+++ src/models.py
@@ -0,0 +1,34 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ChessEval(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        # Ad ogni strato convoluzionale aggiungiamo profondità, ma grazie a kernel = 3x3
+        # e padding = 1 manteniamo la dimensione della schacchiera 8x8
+        self.conv1 = nn.Conv2d(18, 64, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(64)
+
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(128)
+
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(256)
+
+        self.fc1 = nn.Linear(256*8*8, 1024)
+        self.fc2 = nn.Linear(1024, 128)
+        self.fc3 = nn.Linear(128, 1)
+    
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+
+        x = torch.flatten(x, 1)
+
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+        
