3bf764855bfe89172f27922aa299e11e00c8cf43
piergiorgiodanti <piergiorgio.danti@edu.unifi.it>
2026-02-22 10:47:33 +0100

    ChessEncoder

---
+++ main.py
@@ -0,0 +1,102 @@
+import pygame
+import chess
+import torch
+from src.data_utils import ChessEncoder
+
+# Inizzalizzazione Pygame
+pygame.init()
+WITDH, HEIGHT = 600, 600 # Dimensioni finestra
+BOARD_SIZE = 600
+SQ_SIZE = BOARD_SIZE // 8
+FPS = 60
+IMAGES = {}
+
+WHITE = (235, 235, 208)
+GREEN = (200, 99, 0)
+
+def load_images():
+    """Carica le immagini e le adatta alla dimensione delle case"""
+    pieces = ['wp', 'wr', 'wn', 'wb', 'wk', 'wq', 'bp', 'br', 'bn', 'bb', 'bk', 'bq']
+    for piece in pieces:
+        IMAGES[piece] = pygame.transform.scale(pygame.image.load(f"images/{piece}.png"), (SQ_SIZE, SQ_SIZE))
+
+def draw_board(screen):
+    """Disegna la griglia 8x8 sullo schermo"""
+    for row in range(8):
+        for col in range(8):
+            color = WHITE if ((row + col) % 2 == 0) else GREEN
+            pygame.draw.rect(screen, color, (col * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+def draw_pieces(screen, board):
+    """Disegna i pezzi basandosi sullo stato della board"""
+    for row in range(8):
+        for col in range(8):
+            # pygame disegna dall'alto 0 in basso 7
+            square = chess.square(col, 7 - row)
+            
+            # prende il pezzo in posizione (row, col) da board
+            piece = board.piece_at(square)
+           
+            if piece is not None:
+                # nome del pezzo preso
+                color_prefix = 'w' if piece.color == chess.WHITE else 'b'
+                piece_key = color_prefix + piece.symbol().lower()
+
+                # disegna il pezzo sulla scacchiera
+                screen.blit(IMAGES[piece_key], (col * SQ_SIZE, row * SQ_SIZE) )
+
+def handle_move(board, current_selected_sq):
+    pos = pygame.mouse.get_pos()
+    col = pos[0] // SQ_SIZE
+    row = pos[1] // SQ_SIZE
+
+    clicked_sq = chess.square(col, 7 - row)
+
+    if current_selected_sq is None: # il primo click seleziona il pezzo
+        if board.piece_at(clicked_sq): # se c'è un pezzo nella casa selezionata
+            return clicked_sq 
+    else: # secondo click seleziona la casa di arriva
+        move = chess.Move(current_selected_sq, clicked_sq) # si prova a fare la mossa
+
+        if move in board.legal_moves: # se è legale si effettua
+            board.push(move)
+        
+        # se è una promozione:
+        elif chess.Move(current_selected_sq, clicked_sq, promotion=chess.QUEEN) in board.legal_moves:
+            board.push(chess.Move(current_selected_sq, clicked_sq, promotion=chess.QUEEN))
+            
+        # vediamo se la codifica del tensore gasa
+        encode = ChessEncoder()
+        x,y = encode.process_entry(board.fen(),400)
+
+        return None # per la prossima mossa
+    
+    return current_selected_sq
+
+def main():
+    screen = pygame.display.set_mode((WITDH, HEIGHT))
+    pygame.display.set_caption("chess eval")
+    clock = pygame.time.Clock()
+    board = chess.Board()
+    load_images()
+    selected_sq = None # casa selezionata per prima
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                selected_sq = handle_move(board, selected_sq)
+
+        draw_board(screen)
+        draw_pieces(screen, board)
+        pygame.display.flip()
+        clock.tick(FPS)   
+
+        
+    
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
---
+++ src/data_utils.py
@@ -0,0 +1,131 @@
+import torch
+
+from enum import IntEnum
+
+class BoardLayer(IntEnum):
+    # Pezzi Bianchi
+    WHITE_P = 0; WHITE_N = 1; WHITE_B = 2; WHITE_R = 3; WHITE_Q = 4; WHITE_K = 5
+    # Pezzi Neri
+    BLACK_P = 6; BLACK_N = 7; BLACK_B = 8; BLACK_R = 9; BLACK_Q = 10; BLACK_K = 11
+    # Metadati
+    TURN = 12
+    CASTLE_K = 13; CASTLE_Q = 14; CASTLE_k = 15; CASTLE_q = 16
+    EN_PASSANT = 17
+
+class ChessEncoder:
+    
+    def __init__(self):
+        self.piece_to_index = {
+            'P': BoardLayer.WHITE_P, 'N': BoardLayer.WHITE_N, 'B': BoardLayer.WHITE_B,
+            'R': BoardLayer.WHITE_R, 'Q': BoardLayer.WHITE_Q, 'K': BoardLayer.WHITE_K,
+            'p': BoardLayer.BLACK_P, 'n': BoardLayer.BLACK_N, 'b': BoardLayer.BLACK_B,
+            'r': BoardLayer.BLACK_R, 'q': BoardLayer.BLACK_Q, 'k': BoardLayer.BLACK_K
+        }
+    
+    def process_entry(self, fen: str, score_cp: float):
+        
+        x = self.encode(fen)
+        y = self.normalize_score(score_cp)
+
+        return x,y
+
+    def encode(self, fen: str):
+        """
+        Converte una stringa FEN (Forsyth–Edwards Notation), che descrive una
+        configurazione di una partita di scacchi, in un tensore 8x8x18.
+
+        Formato FEN:
+        - La prima sezione contiene 8 stringhe separate dal carattere '/',
+        ciascuna rappresentante una traversa della scacchiera dalla 8ª alla 1ª.
+        - All'interno di ogni traversa, i pezzi sono elencati da sinistra a destra
+        (colonne da 'a' a 'h').
+        - Le lettere maiuscole rappresentano i pezzi bianchi (P, N, B, R, Q, K),
+        le lettere minuscole rappresentano i pezzi neri (p, n, b, r, q, k).
+        - Le case vuote sono indicate da un numero (1–8) che specifica quante
+        case vuote consecutive sono presenti.
+        - Dopo la disposizione dei pezzi, sono indicati:
+            * Il turno di gioco ('w' per Bianco, 'b' per Nero).
+            * I diritti di arrocco ('KQkq'):
+                - 'K': arrocco corto Bianco
+                - 'Q': arrocco lungo Bianco
+                - 'k': arrocco corto Nero
+                - 'q': arrocco lungo Nero
+            * La casa di en passant (es. 'e3') oppure '-' se non disponibile.
+            * (Eventuali campi successivi come halfmove clock e fullmove number
+            possono essere presenti ma non sono rilevanti per questa codifica.)
+
+        Struttura del tensore di output (8x8x18):
+        - Canali 0–5:   pezzi bianchi (nell’ordine: Pedone, Cavallo, Alfiere,
+                        Torre, Donna, Re).
+        - Canali 6–11:  pezzi neri (stesso ordine dei bianchi).
+        - Canale 12:    turno di gioco (1 se tocca al Bianco, 0 se al Nero).
+        - Canale 13:    diritto di arrocco corto Bianco (1 se disponibile, 0 altrimenti).
+        - Canale 14:    diritto di arrocco lungo Bianco (1 se disponibile, 0 altrimenti).
+        - Canale 15:    diritto di arrocco corto Nero (1 se disponibile, 0 altrimenti).
+        - Canale 16:    diritto di arrocco lungo Nero (1 se disponibile, 0 altrimenti).
+        - Canale 17:    casa di en passant (codificata opportunamente sulla griglia,
+                        oppure tutta a zero se non disponibile).
+
+        Parametri
+        ----------
+        fen : str
+            Stringa FEN che descrive lo stato corrente della partita.
+
+        Ritorna
+        -------
+        tensor : array-like (8, 8, 18)
+            Rappresentazione tensoriale dello stato della scacchiera.
+        """
+        parts = fen.split(' ')
+        board_part = parts[0]     
+        turn = parts[1]           
+        castling = parts[2]       
+        en_passant = parts[3]     
+
+        # Inizializziamo il tensore (Canali, Righe, Colonne)
+        tensor = torch.zeros((18, 8, 8), dtype=torch.float32)
+
+        # 1. Riempimento pezzi
+        rows = board_part.split('/')
+        for i, row in enumerate(rows):
+            col = 0
+            for char in row:
+                if char.isdigit():
+                    col += int(char)
+                else:
+                    p_idx = self.piece_to_index[char]
+                    tensor[p_idx, i, col] = 1
+                    col += 1
+
+        # 2. Codifica Turno
+        if turn == 'w':
+            tensor[BoardLayer.TURN, :, :] = 1
+
+        # 3. Codifica Arrocco
+        castling_map = {
+            'K': BoardLayer.CASTLE_K, 
+            'Q': BoardLayer.CASTLE_Q, 
+            'k': BoardLayer.CASTLE_k, 
+            'q': BoardLayer.CASTLE_q
+        }
+        for char in castling:
+            if char in castling_map:
+                tensor[castling_map[char], :, :] = 1
+
+        # 4. Codifica En Passant
+        if en_passant != '-':
+            ep_col = ord(en_passant[0]) - ord('a')
+            ep_row = 8 - int(en_passant[1])
+            # Controlliamo che le coordinate siano nei limiti (sicurezza)
+            if 0 <= ep_row < 8 and 0 <= ep_col < 8:
+                tensor[BoardLayer.EN_PASSANT, ep_row, ep_col] = 1
+
+        return tensor 
+    
+    def normalize_score(self, score_cp):
+        """
+        Schiaccia il punteggio centipawn in un range [-1, 1].
+        """
+        return torch.tanh(torch.tensor(score_cp / 400.0, dtype=torch.float32))
+
+
